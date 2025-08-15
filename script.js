735e184c2fd81fe7ba1e40af99c2560f469c82fb
kaelzincl7 <148562069+kaelzincl7@users.noreply.github.com>
2025-08-15 10:11:53 -0300

    parte 4

--- script.js
+++ script.js
@@ -51,6 +51,12 @@ function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 
 mostraPergunta();
-
